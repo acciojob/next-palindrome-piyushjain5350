110c16322da504c7350133629c58c2afbe34b019
Piyush Jain <126599802+piyushjain5350@users.noreply.github.com>
2023-11-29 23:21:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function nextPalindrome(num) {
   //your JS code here.
+	 while (true) {
+    num++;
+    if (isPalindrome(num)) {
+      return num;
+    }
+  }
 }
+function isPalindrome(number) {
+  const str = String(number);
+  const len = str.length;
+  for (let i = 0; i < len / 2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
 
 const input = prompt("Enter a palindrome number");
 alert(nextPalindrome(input));
